a81f0dc3bd69cf49eaba1397384ef4b9941e82fc
phalgunendra <61001244+phalgunendra@users.noreply.github.com>
2026-05-04 18:56:13 -0500

    Delete Anurag directory

--- Anurag/src/com/capgemini/AnuragDemo.java
+++
@@ -1,9 +0,0 @@
-package com.capgemini;
-
-public class AnuragDemo {
-public static void main(String[] args) {
-	System.out.println("teja");
-	System.out.println("new");
-	System.out.println("dsjkagjdgajd");
-}
-}
